e192f06d4f911d2773799c98c355c76b5961d625
Harika Sahitya <harikapeddinti06@gmail.com>
2025-07-25 21:54:32 +0530

    Add files via upload

---
+++ ArithmeticOperations.java
@@ -0,0 +1,12 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        int a = 20, b = 10;
+
+        System.out.println("First number: " + a);
+        System.out.println("Second number: " + b);
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+    }
+}
