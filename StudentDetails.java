13b3a2ba500f41cfab974e443377093358b62c86
Harika Sahitya <harikapeddinti06@gmail.com>
2025-07-25 21:59:46 +0530

    Add files via upload

---
+++ StudentDetails.java
@@ -0,0 +1,12 @@
+public class StudentDetails {
+    public static void main(String[] args) {
+        String name = "Harika";
+        int rollNo = 101;
+        String department = "Computer Science";
+
+        System.out.println("Student Details:");
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNo);
+        System.out.println("Department: " + department);
+    }
+}
